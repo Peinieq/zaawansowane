adb20cb9611e92d88a40e58f72583ffba744b0b5
Sylwester <mozartuss@protonmail.com>
2019-10-09 18:50:18 +0200

    usuniecie ostatniej linijki

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,6 +4,5 @@ public class App {
     public static void main(String... args) {
         System.out.println("Hello world");
         System.out.println("Cos tam jeszcze");
-        System.out.println("Cos tam jeszcze");
     }
 }
